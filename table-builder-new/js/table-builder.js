abb7335f1c562e1e1c3e7953f369150e5e21a07e
Stu Furlong <stu.furlong@americaneagle.com>
2017-03-01 16:08:54 -0600

    Updated javascript

--- table-builder-new/js/table-builder.js
+++ table-builder-new/js/table-builder.js
@@ -1,25 +1,41 @@
 $(function(){
 
-    var builderWrap = $('.table-builder-wrap');
-    var tablePrompt = $('<div class="table-prompt"></div>');
-    var tablePathStepOne = $('<div class="table-path step-one"></div>');
-    var tablePathStepTwo = $('<div class="table-path step-two"></div>');
 
+    // ** CONSTANTS ** //
+    var builderWrap = $('.table-builder-wrap'),
+        tablePrompt = $('<div class="table-prompt"></div>'),
+        tablePathStepOne = $('<div class="table-path step-one"></div>'),
+        tablePathStepTwo = $('<div class="table-path step-two"></div>');
+
+
+
+    // ** TABLE BUILD ** //
     function tableBuild() {
+        var tableBuilder = $('<div class="table-builder"></div>');
 
+        tableBuilder.append('<button class="add-col button">Add Column</button>' +
+                            '<button class="add-row button">Add Row</button>' +
+                            '<button class="save button">Save</button>' +
+                            '<div class="controls"><button class="button merge-group">Merge Column Cells</button><button class="button merge-across">Merge Row Cells</button></div>' +
+                            '<div class="columns"></div><div class="rows"></div>');
+          
+        builderWrap.append(tableBuilder);
+        builderWrap.append('<table id="preview-table"><thead><tr></tr></thead><tbody></tbody></table>');
     }
 
-    function tableCreate(type) {
 
+
+    // ** TABLE BUILD SELECTION ** //
+    function tableCreate(type) {
         if(type == "new") {
             builderWrap.empty();
-            builderWrap.append('New Table')
+            tableBuild();
         } else if (type == "import") {
             tablePathStepOne.hide();
-            tablePathStepTwo.append('<button class="back">Back</button><br />')
-                            .append('<h1>Import JSON</h1>')
-                            .append('<textarea></textarea><br />')
-                            .append('<button class="button import-submit">Import</button>');
+            tablePathStepTwo.append('<button class="back">Back</button><br />' +
+                                    '<h1>Import JSON</h1>' +
+                                    '<textarea></textarea><br />' +
+                                    '<button class="button import-submit">Import</button>');
             tablePrompt.append(tablePathStepTwo);
         } else if (type == "wizard") {
             tablePath.empty();
@@ -29,12 +45,14 @@ $(function(){
         }
     }
 
-    function tableStart() {
 
-        tablePathStepOne.append('<h1>Create a new table</h1>')
-                        .append('<button class="button new-table">New Table</button>')
-                        .append('<button class="button import-table">Import Table</button>')
-                        .append('<button class="button table-wizard">Table Wizard</button>');
+
+    // ** START SCREEN ** //
+    function tableStart() {
+        tablePathStepOne.append('<h1>Create a new table</h1>' +
+                                '<button class="button new-table">New Table</button>' +
+                                '<button class="button import-table">Import Table</button>' +
+                                '<button class="button table-wizard">Table Wizard</button>');
 
         tablePrompt.append(tablePathStepOne);
         builderWrap.append(tablePrompt);
@@ -52,11 +70,415 @@ $(function(){
         });
     }
 
+
+
+    // ** BACK BUTTON** //
     $('body').on('click', '.back', function(){
         tablePathStepTwo.empty().remove();
         tablePathStepOne.show();
     });
 
+
+
+    // ** INIT ** //
     tableStart();
 
+    
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+    
+
+    /***** CONSTANTS *****/
+
+    var addColBtn = $('.add-col'),
+        addRowBtn = $('.add-row'),
+    
+        columnGroup = $('.columns'),
+        rowGroup = $('.rows'),
+            
+        previewTable = $('#preview-table'),
+        previewTableHead = previewTable.children('thead'),
+        previewTableBody = previewTable.children('tbody');
+    
+    var tableJSON = [],
+        JSONoutput;
+    var columnJson = [];
+    var rowJson = [];
+    var columnParent = { 'Columns': columnJson };
+    var rowParent = { 'Rows': rowJson };
+    
+
+
+    /***** ADD COLUMN *****/
+    
+    builderWrap.on('click', '.add-col', function(e) {
+
+        var columnControl = $('.column'),
+            rowControl = $('.row');
+        
+        var colCount = columnControl.length + 1,
+            rowCount = rowControl.length;
+        
+        var column = '<div class="column" id="col' + colCount + '">' +
+                    '<p>Column ' + colCount + '</p>' +
+                    '<input type="text" data-col="' + colCount + '" /></div>';
+        
+        $('.columns').append(column);
+        
+        var columnJsonItem = { 'column': colCount };
+        
+        columnJson.push(columnJsonItem)
+
+        
+        $('#preview-table thead tr').append('<th data-col="' + colCount + '">Column ' + colCount + '</th>');
+
+        $('#preview-table tbody tr:not(.full-row)').each(function() {
+            var $this = $(this),
+                spot = $this.index() + 1;
+            
+            $this.append('<td data-col="' + colCount + '" data-row="' + spot + '">Col ' + colCount + ' - Row ' + spot + '</td>');
+        });
+
+        $('#preview-table tbody tr.full-row').each(function() {
+            $(this).find('td').attr('colspan', colCount);
+        });
+        
+        rowControl.each(function() {
+            var $this = $(this),
+                rowid = $this.attr('id').replace('row', '');
+
+            if(!$this.hasClass('full-row')){
+                $this.find('.row-cells').append('<div class="cell"' + ' data-col="' + colCount + '" data-row="' + rowid + '">' +
+                            '<input type="text" class="field-edit"' +
+                            'data-col="' + colCount + '" data-row="' + rowid + '" placeholder="" />' + 
+                            //'<div><a href="#" class="merge-down">Merge Down</a></div>' +
+                            '</div>');
+            }
+        });
+    }); 
+    
+        
+    /***** ADD ROW *****/
+    
+    builderWrap.on('click', '.add-row', function(e) {
+        var columnControl = $('.column'),
+            rowControl = $('.row');
+        
+        var rowCount = rowControl.length + 1;
+        var row = '<div class="row" id="row' + rowCount + '">' +
+                //'<p>Row ' + rowCount + '</p>' +
+                //'<div><label><input type="checkbox" class="full" /> Full row</label></div>' +
+                '<div class="row-cells"></div></div>';
+
+        $('.rows').append(row);
+        
+        var rowJsonItem = { 'row': rowCount };
+        
+        rowJson.push(rowJsonItem)
+
+        var tr = $('<tr data-row="' + rowCount + '"></tr>');
+        var currowCount = columnControl.length;
+        
+        for(var i = 0; i < currowCount; i++) {
+            var colnum = i + 1;
+            var colName = $('.column input[data-col="' + colnum + '"]').val();
+            var input = $('<div class="cell"' + ' data-col="' + colnum + '" data-row="' + rowCount + '">' +
+                            '<input type="text" class="field-edit"' +
+                            'data-col="' + colnum + '" data-row="' + rowCount + '" placeholder="' + colName + '" />' + 
+                            //'<div><a href="#" class="merge-down">Merge Down</a></div>' +
+                            '</div>');
+
+            $('#row' + rowCount + ' .row-cells').append(input);
+
+            tr.append('<td data-col="' + colnum + '" data-row="' + rowCount + '">Col ' + colnum + ' - Row ' + rowCount + '</td>');
+        }
+        
+        previewTableBody.append(tr);
+    });
+    
+    
+    $('.save').on('click', function(){
+        tableJSON.push(columnParent, rowParent);
+        var JSONoutput = JSON.stringify(tableJSON);
+        $('.json-output').html(JSONoutput);
+    });
+
+    
+    
+    /***** UPDATING COLUMN TEXT *****/
+    
+    $('.columns').on('keyup', 'input', function() {
+        var $this = $(this),
+            col = $this.data('col'),
+            text = $this.val();
+        
+        $('th[data-col="' + col + '"]').text(text);
+        $('.row input[data-col="' + col + '"]').attr('placeholder', text)
+    });
+    
+
+
+    /***** UPDATING CELL TEXT *****/
+
+    $('.rows').on('keyup', '.field-edit', function() {
+        var $this = $(this),
+            row = $this.data('row'),
+            col = $this.data('col'),
+            val = $this.val();
+        
+        $('td[data-col="' + col + '"][data-row="' + row +'"]').text(val);
+    });
+
+    $('.rows').on('click', '.cell', function() {
+        $(this).find('input').focus();
+    });
+
+
+
+    /***** MAKE FULL ROW *****/
+
+    $('.rows').on('change', '.full', function() {
+        var $this = $(this),
+            parentRow = $this.closest('.row'),
+            rowID = parentRow.attr('id').replace('row', ''),
+            colLength = $('.column').length,
+            addedRow = $('tr[data-row="' + rowID + '"]');
+
+        if($this.is(':checked')){
+            addedRow.addClass('full-row');
+            addedRow.find('td:not(:first-child)').hide();
+            addedRow.find('td:first-child').attr('colspan', colLength);
+
+            parentRow.addClass('full-row');
+            parentRow.find('.merge-down').hide();
+            parentRow.prev().find('.merge-down').hide();
+
+            $('#row' + rowID + ' .cell:not(:first-child)').hide();
+
+        } else {
+            var columnControl = $('.column'),
+                rowControl = $('.row');
+
+            var rowCount = rowControl.length + 1,
+                currowCount = columnControl.length;
+
+            $('#row' + rowID + ' .cell:not(:first-child)').show();
+            $('tr[data-row="' + rowID + '"] td:first-child').removeAttr('colspan');
+
+            addedRow.removeClass('full-row');
+            addedRow.find('td:not(:first-child)').show();
+
+            parentRow.find('.merge-down').show();
+        }
+    });
+
+
+
+    /***** MERGE DOWN *****/
+    
+    $('.rows').on('click', '.merge-down', function(e) {
+        e.preventDefault();
+
+        var $this = $(this),
+            cellInput = $this.parent().prev(),
+            thisRow = $this.closest('.row'),
+            nextRow = thisRow.next('.row');
+
+        var colNum = cellInput.data('col'),
+            rowNum = cellInput.data('row'),
+            rowNumNext = cellInput.data('row') + 1,
+            TD = $('td[data-col="' + colNum + '"][data-row="' + rowNum + '"]');
+
+        var nextRowCol = nextRow.find('input[data-col="' + colNum + '"]');
+
+        $('tr[data-row="' + rowNum + '"]').next().find('td[data-col="' + colNum + '"][data-row="' + rowNumNext + '"]').hide();
+
+        var nextMerged = $('td[data-col="' + colNum + '"][data-row="' + rowNumNext + '"]').attr('rowspan');
+        var mergedNum = Number(nextMerged) + 1;
+
+        $this.attr('rowspan') ? TD.attr('rowspan', mergedNum) : TD.attr('rowspan', '2');
+
+        thisRow.find('label').hide();
+        nextRow.find('label').hide();
+
+        nextRowCol.attr('disabled', 'disabled');
+        //nextRowCol.next().hide();
+
+        if(nextRow.length > 0) {
+            $this.hide();
+        } 
+    });
+
+
+
+    /***** MERGE ACROSS *****/
+    
+    $('.controls').on('click', '.merge-across', function(e) {
+        e.preventDefault();
+
+        var firstitem = $('.highlighted').first();
+        var mergedNum = $('.highlighted').length;
+
+        var colNum = firstitem.data('col'),
+            rowNum = firstitem.data('row');
+
+        var TD = $('td[data-col="' + colNum + '"][data-row="' + rowNum + '"]');
+
+        $(this).attr('rowspan', mergedNum);
+
+        TD.attr('colspan', mergedNum);
+
+        firstitem.nextUntil('.cell:not(.highlighted)').addClass('cellhidden');
+
+        $('.highlighted.cellhidden').each(function(){
+
+            var $this = $(this),
+                thiscolNum = $this.data('col'),
+                thisrowNum = $this.data('row');
+
+            $('td[data-col="' + thiscolNum + '"][data-row="' + thisrowNum + '"]').hide();
+        });
+    });
+
+
+
+    /***** RIGHT CLICK FUNCTIONALITY *****/
+
+    var isMouseDown = false,
+        initialColPos, initialRowPos;
+//
+    $('.rows').on('mousedown', '.cell', function () {
+        switch (event.which) {
+            case 1:
+
+            var $this = $(this);
+
+            $('.highlight-focus').removeClass('highlight-focus');
+            $('.highlighted').removeClass("highlighted");
+            $('.highlightable').removeClass('highlightable');
+            $('.merge-row').removeClass('merge-row');
+            $('.hidden').removeClass('hidden');
+
+            if(event.shiftKey) {
+                isMouseDown = true;
+                if ($('.highlighted').length === 0){
+                    initialColPos = $this.data('col');
+                    initialRowPos = $this.data('row');
+                    $this.addClass("highlighted first-highlight");
+                } else {
+                    $this.addClass("highlighted");
+                }
+            }
+            return false; 
+            break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+        }
+    }).on('mouseover', '.cell', function () {
+        var $this = $(this),
+            highlighted = $('.highlighted'),
+            previewTable = $('#previewTable');
+
+        if (isMouseDown = true && event.shiftKey) {
+            if(highlighted.length === 1){
+                $this.addClass("highlighted");
+
+                var mergeColBtn = $('.merge-group'),
+                    mergeRowBtn = $('.merge-across');
+
+                if($this.data('col') === initialColPos) {
+                    $('.cell[data-col="' + initialColPos + '"]').addClass('highlightable');
+                    previewTable.addClass('merge-col');
+                    mergeColBtn.show();
+                    mergeRowBtn.hide();
+                } else if($this.data('row') === initialRowPos){
+                    $('.cell[data-row="' + initialRowPos + '"]').addClass('highlightable');
+                    $('#row'+ initialRowPos).addClass('highlighted-row')
+                    previewTable.addClass('merge-row');
+                    mergeRowBtn.show();
+                    mergeColBtn.hide();
+                } else { }
+            } else if (highlighted.length > 1) {
+                if($this.hasClass('highlightable')){
+                    $this.addClass("highlighted");
+                }
+            } else { }
+        }
+    }).on('mouseup', function () {
+        isMouseDown = false;
+    });
+
+    $(document).on('mousedown', function(){
+        switch (event.which) {
+            case 1:
+            isMouseDown = false;
+            break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+        }
+    });
+
+    function mergeCol(){
+
+    }
+
+    $('.table-builder').on('contextmenu', '.highlighted', function(e){
+        e.preventDefault();
+
+        $('.table-builder').addClass('highlight-focus');
+        $('.controls').show();
+    });
+
+    $('.merge-group').on('click', function(e){
+        var highlighted = $(this),
+            first = highlighted.first(),
+            firstRow = first.data('row'),
+            firstCol = first.data('col'),
+            highlightedItems = $('.highlighted'),
+            allHighlights = $('.highlighted').length;
+
+        var first_iteration = true;
+
+        highlightedItems.each(function(){
+            var $this = $(this),
+                thisRow = $this.data('row'),
+                thisCol = $this.data('col'),
+                allCols = [],
+                allRows = [],
+                TD = $('td[data-row="' + thisRow + '"][data-col="' + thisCol + '"]');
+
+            if (first_iteration == true) {
+                $('.cell.highlighted:not(.first-highlight)').addClass('cellhidden')
+                TD.addClass('highlighted').attr('rowspan', allHighlights);
+                allCols.push(thisCol);
+                allRows.push(thisRow);
+                first_iteration = false;
+            } else {
+                TD.addClass('highlighted').hide();
+                allCols.push(thisCol);
+                allRows.push(thisRow);
+            } 
+        });
+    });
+
 });
+
+
